f669d84efea65a9981de41aa9c65f91479f50500
SuhwanCha <suhw4n@gmail.com>
2022-11-28 21:09:03 +0900

    lint

--- test/marker_test.dart
+++ test/marker_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:flutter_test/flutter_test.dart';
