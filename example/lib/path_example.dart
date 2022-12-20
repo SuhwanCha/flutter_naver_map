7e089ad151190a348395d858a6821a64b5238682
SuhwanCha <suhw4n@gmail.com>
2022-12-20 20:17:24 +0900

    geojson

--- example/lib/path_example.dart
+++ example/lib/path_example.dart
@@ -19,25 +19,31 @@ class _PathExampleState extends State<PathExample> {
     DefaultAssetBundle.of(context)
         .loadString('assets/TL_SCCO_SIG.json')
         .then((data) {
+      var i = 0;
       final jsonResult = jsonDecode(data) as Map<String, dynamic>;
       final features = jsonResult['features'] as List<dynamic>;
       for (final element in features) {
-        polygon.add(
-          PolygonOverlay(
-            ((element as Map<String, dynamic>)['properties']
-                as Map<String, dynamic>)['SIG_CD'] as String,
-            (((element['geometry'] as Map<String, dynamic>)['coordinates']
-                    as List<dynamic>)[0] as List<dynamic>)
-                .map(
-                  (e) =>
-                      LatLng((e as List<dynamic>)[1] as double, e[0] as double),
-                )
-                .toList(),
-            outlineColor: Colors.indigoAccent,
-            outlineWidth: 6,
-            color: Colors.black54,
-          ),
-        );
+        element as Map<String, dynamic>;
+
+        final geometry = element['geometry'] as Map<String, dynamic>;
+
+        final coords = geometry['coordinates'] as List<dynamic>;
+
+        for (final elem in coords) {
+          polygon.add(
+            PolygonOverlay(
+              // (element['properties'] as Map<String, dynamic>)['SIG_ENG_NM']
+              //     as String,
+              '${i++}',
+              (elem as List<dynamic>)
+                  .map((e) => LatLng(e[1] as double, e[0] as double))
+                  .toList(),
+              outlineColor: Colors.indigoAccent,
+              outlineWidth: 3,
+              color: Colors.white.withOpacity(0.2),
+            ),
+          );
+        }
       }
 
       setState(() {});
@@ -49,14 +55,14 @@ class _PathExampleState extends State<PathExample> {
     final controller = NaverMapController();
     return NaverMap(
       controller: controller,
-      options: const NaverMapOptions(
+      options: NaverMapOptions(
         layers: [],
-        initialCameraPosition: CameraPosition(
+        initialCameraPosition: const CameraPosition(
           target: LatLng(37.56823358823172, 126.9838688358965),
-          zoom: 16,
+          zoom: 6,
         ),
-        // mapType: MapType.navi,
-        // nightModeEnabled: true,
+        mapType: MapType.navi,
+        nightModeEnabled: true,
       ),
       polygons: polygon,
     );
